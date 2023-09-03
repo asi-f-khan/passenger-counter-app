3618774587e69bb15ac4fdae600ea8b612c433cd
Asif Khan <101961779+asi-f-khan@users.noreply.github.com>
2023-09-03 10:39:50 +0530

    Minor Changes in JS files

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-
-let count =0;
-let cnt=0;
-let cntel= document.getElementById("count-el");
-let precount= document.getElementById("pre-cnt");
+let count = 0;
+let cnt = 1;
+let cntel = document.getElementById("count-el");
+let precount = document.getElementById("pre-cnt");
 
 function increment() {
-  count +=1;
-  cntel.innerText=count;
+  count += 1;
+  cntel.innerText = count;
 }
 
 function reset() {
-  count=0;
-  cntel.innerText=0;
-  cnt=0;
-  precount.innerText= "Previous Count:"
+  count = 0;
+  cntel.innerText = 0;
+  cnt = 1;
+  precount.innerText = "Previous Count:";
 }
 
 function decrement() {
-  if(count!=0){
-    count-=1;
-cntel.innerText=count;
-
+  if (count != 0) {
+    count -= 1;
+    cntel.innerText = count;
   }
-
-
 }
 
-function save(){
-
-  if(count!=0){
-    precount.innerText= precount.innerText+" "+cnt+" -> "+count+" , ";
-    cnt+=1;
-    count=0;
-    cntel.innerHTML=count;
+function save() {
+  if (count != 0) {
+    precount.innerText =
+      precount.innerText + " " + cnt + " -> " + count + " , ";
+    cnt += 1;
+    count = 0;
+    cntel.innerHTML = count;
   }
-
-   
 }
